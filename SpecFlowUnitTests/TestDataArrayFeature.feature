8c7fd37aa80241d1020fccb4a726b94f4d6dc9cc
gyomihaly <gyomihaly@gmail.com>
2017-05-03 23:11:26 +0200

    Több bemenetű specflow test

---
+++ SpecFlowUnitTests/TestDataArrayFeature.feature
@@ -0,0 +1,12 @@
+﻿Feature: TestDataArrayFeature
+	Test for multiple possibilities
+
+@mytag
+Scenario Outline: Test for multiple expressions and inputs
+	Given I have added "<expr>" to the expression
+	Then searching in "<searched>" , the result should be '<res>'
+
+	Examples:
+	| expr | searched   | res   |
+	| .com | googlecom  | false |
+	| .com | google.com | true  |
---
+++ SpecFlowUnitTests/TestDataArrayFeatureSteps.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using CSharpVerbalExpressions;
+using NUnit.Framework;
+
+namespace SpecFlowUnitTests
+{
+    [Binding]
+    public class TestDataArrayFeatureSteps
+    {
+        private VerbalExpressions verb = VerbalExpressions.DefaultExpression;
+
+        [Given(@"I have added ""(.*)"" to the expression")]
+        public void GivenIHaveAddedToTheExpression(string p0)
+        {
+            verb.Add(p0);
+        }
+        
+        [Then(@"searching in ""(.*)"" , the result should be '(.*)'")]
+        public void ThenSearchingInTheResultShouldBe(string p0, string p1)
+        {
+            if (Convert.ToBoolean(p1))
+            {
+                Assert.IsTrue(verb.IsMatch(p0));
+            }
+            else
+            {
+                Assert.IsFalse(verb.IsMatch(p0));
+            }
+        }
+    }
+}
