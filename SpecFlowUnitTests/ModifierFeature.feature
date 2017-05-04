bc78be325694fb2418951dc655fd576901afa810
gyomihaly <gyomihaly@gmail.com>
2017-05-04 21:20:52 +0200

    Test modifier features

---
+++ SpecFlowUnitTests/ModifierFeature.feature
@@ -0,0 +1,12 @@
+﻿Feature: ModifierFeature
+	Lets's test the modifiers
+
+
+Scenario Outline: Using modifier
+	Given I have added "<expr>" to the expressions
+	Then with "<modifier>" modifier, it should match "<res>"
+
+	Examples:
+	| expr        | modifier | res                  |
+	| teststring  | i        | TESTSTRING           |
+	| test string | x        | test string #comment |
---
+++ SpecFlowUnitTests/ModifierFeatureSteps.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using CSharpVerbalExpressions;
+using NUnit.Framework;
+
+namespace SpecFlowUnitTests
+{
+    [Binding]
+    public class ModifierFeatureSteps
+    {
+        private VerbalExpressions verb = VerbalExpressions.DefaultExpression;
+        [Given(@"I have added ""(.*)"" to the expressions")]
+        public void GivenIHaveAddedToTheExpressions(string p0)
+        {
+            verb.Add(p0);
+        }
+        
+        [Then(@"with ""(.*)"" modifier, it should match ""(.*)""")]
+        public void ThenWithModifierItShouldMatch(char p0, string p1)
+        {
+            verb.AddModifier(p0);
+            Assert.IsTrue(verb.IsMatch(p1));
+        }
+    }
+}
